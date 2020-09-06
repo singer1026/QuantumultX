a1d403c3ed98afaffa2df13a8df43eb140ce92ac
嗷大喵 <singer1026@gmail.com>
2020-09-06 22:53:27 +0800

    Update hesz.js
    1

--- hesz.js
+++ hesz.js
@@ -2,7 +2,7 @@
 洪恩识字
 *.ihuman.com
 https://shizibook.ihuman.com/api/v1/getRadical
-^https?:\/\/.*\.ihuman\.com\/api\/v1\/getRadical(.+) url script-response-body https://raw.githubusercontent.com/singer1026/QuantumultX/master/hesz.js
+^https?:\/\/.*\.ihuman\.com\/api\/(.+) url script-response-body https://raw.githubusercontent.com/singer1026/QuantumultX/master/hesz.js
 */
 var obj = JSON.parse($response.body);
 if ($request.url.indexOf("/api/v1/getRadical") != -1){
@@ -13,5 +13,4 @@ if ($request.url.indexOf("/api/v1/getRadical") != -1){
   }  
 }
 var body = JSON.stringify(obj);
-console.log("洪恩识字========================\n" + body);
 $done(body);
