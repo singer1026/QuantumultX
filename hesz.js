8ac2bf90a94c475548adc5ac4f5779500fe4c611
嗷大喵 <singer1026@gmail.com>
2020-09-06 23:35:41 +0800

    Update hesz.js
    1

--- hesz.js
+++ hesz.js
@@ -4,6 +4,8 @@
 https://shizibook.ihuman.com/api/v1/getRadical
 洪恩双语绘本
 https://bookapi.ihuman.com/v3/get_book_info
+洪恩数学
+https://mathapi.ihuman.com/v3/get_purchase_list
 ^https?:\/\/.*\.ihuman\.com\/(.+) url script-response-body https://raw.githubusercontent.com/singer1026/QuantumultX/master/hesz.js
 */
 var obj = JSON.parse($response.body);
@@ -21,6 +23,11 @@ if ($request.url.indexOf("bookapi.ihuman.com/v3/get_book_info") != -1) {
     	var item = list[i];
     	item.is_vip = 0;
   	} 
+}
+
+if ($request.url.indexOf("mathapi.ihuman.com/v3/get_purchase_list") != -1) {
+	var vip = obj.result.vip;
+	vip.vip_type = 1;
 }
 var body = JSON.stringify(obj);
 $done(body);
